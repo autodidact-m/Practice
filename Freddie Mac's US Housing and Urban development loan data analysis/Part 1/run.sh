61b6f3740d939bc8d5e95575f6d031913bb128a9
Meenal Badki <64068746+autodidact-m@users.noreply.github.com>
2021-08-17 11:46:13 +0530

    Add files via upload

---
+++ Freddie Mac's US Housing and Urban development loan data analysis/Part 1/run.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+python DataIngestion_Part1.py $username $password $startYear $endYear
+
+
+$username = prashantvksingh@gmail.com
+$password = V4wlNZow
+$startYear = 2005
+$endYear = 2009
+
